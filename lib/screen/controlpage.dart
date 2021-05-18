357fed9d9b7b0bb4c9524aaf02ec68199f84ce5a
ledang1234 <le.dang1234@hcmut.edu.vn>
2021-05-19 01:04:43 +0700

    test demo

--- lib/screen/controlpage.dart
+++ lib/screen/controlpage.dart
@@ -1,7 +1,9 @@
 import 'package:multidisproject_2/screen/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_switch/flutter_switch.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
 class ControlPage extends StatefulWidget {
 
   @override
@@ -12,8 +14,22 @@ class _ControlPageState extends State<ControlPage> {
   bool status = false;
   bool status1 = false;
   bool status2 = false;
+  final databaseReferenceTest = FirebaseFirestore.instance.collection('switch').doc('switch1');
+
+
   @override
     Widget build(BuildContext context) {
+    //     return  StreamBuilder(
+    // stream: FirebaseFirestore.instance.collection("switch").snapshots(includeMetadataChanges: true),
+    // builder: (BuildContext context,
+    //           AsyncSnapshot snapshot) {  
+                 
+                // if (snapshot.connectionState == ConnectionState.active){
+                //    print(snapshot.data.docs[0].data()['data']);
+                //     var data = snapshot.data.docs[0].data()['data'];
+                //     if (data ==1){status = true;}
+                //     else {status = false;}
+                // }
       return new MaterialApp(
         home: DefaultTabController(
           length: 4,
@@ -93,6 +109,13 @@ class _ControlPageState extends State<ControlPage> {
                             padding: 8.0,
                             showOnOff: true,
                             onToggle: (val) {
+                               val
+                              ? databaseReferenceTest
+                                  .update({'data': '1'})
+                              : databaseReferenceTest
+                                  .update({'data': '0'}) ;
+                             
+
                               setState(() {
                                 status = val;
                               });
@@ -102,205 +125,12 @@ class _ControlPageState extends State<ControlPage> {
                         ),
                       ],
                       ),
-                      TableRow(children: [
-                        Container (
-                            margin: EdgeInsets.all(10),
-                            padding: EdgeInsets.all(10),
-                            alignment: Alignment.center,
-                            child: Text('Light 2',style: TextStyle( fontSize: 26,))
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          alignment: Alignment.center,
-                          child: FlutterSwitch(
-                            width: 60.0,
-                            height: 25.0,
-                            valueFontSize: 10.0,
-                            toggleSize: 15.0,
-                            value: status1,
-                            borderRadius: 30.0,
-                            padding: 8.0,
-                            showOnOff: true,
-                            onToggle: (val1) {
-                              setState(() {
-                                status1 = val1;
-                              });
-                            },
-                          ),
-
-                        ),
-
-                      ],
-                      ),
-                      TableRow(children: [
-                        Container (
-                            margin: EdgeInsets.all(10),
-                            padding: EdgeInsets.all(10),
-                            alignment: Alignment.center,
-                            child: Text('Speaker',style: TextStyle( fontSize: 26,))
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          alignment: Alignment.center,
-                          child: FlutterSwitch(
-                            width: 60.0,
-                            height: 25.0,
-                            valueFontSize: 10.0,
-                            toggleSize: 15.0,
-                            value: status2,
-                            borderRadius: 30.0,
-                            padding: 8.0,
-                            showOnOff: true,
-                            onToggle: (val1) {
-                              setState(() {
-                                status2 = val1;
-                              });
-                            },
-                          ),
-
-                        ),
-                      ],
-                      )
+                     
+     
                     ],
                   ),
                 ),
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.all(50.0),
-                  child: Table(
-                    //defaultVerticalAlignment: Alignment.center,
-                    //border: TableBorder.all(color: Colors.black),
-                    children: [
-                      TableRow(children: [
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              color: Colors.orange,
-                              border: Border.all(
-                                  color: Colors.orangeAccent, // Set border color
-                                  width: 3.0),   // Set border width
-                              borderRadius: BorderRadius.all(
-                                  Radius.circular(10.0)), // Set rounded corner radius
-                              boxShadow: [BoxShadow(blurRadius: 10,color: Colors.black,offset: Offset(1,3))] // Make rounded corner of border
-                          ),
-                          child: Text("DEVICES",style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold,),),
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              color: Colors.orange,
-                              border: Border.all(
-                                  color: Colors.orangeAccent, // Set border color
-                                  width: 3.0),   // Set border width
-                              borderRadius: BorderRadius.all(
-                                  Radius.circular(10.0)), // Set rounded corner radius
-                              boxShadow: [BoxShadow(blurRadius: 10,color: Colors.black,offset: Offset(1,3))] // Make rounded corner of border
-                          ),
-                          child: Text("STATUS",style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold,),),
-                        ),
-                      ]),
-                      TableRow(children: [
-
-                        Container (
-                            margin: EdgeInsets.all(10),
-                            padding: EdgeInsets.all(10),
-                            alignment: Alignment.center,
-                            child: Text('Light 1',style: TextStyle( fontSize: 26,))
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          alignment: Alignment.center,
-                          child: FlutterSwitch(
-                            width: 60.0,
-                            height: 25.0,
-                            valueFontSize: 10.0,
-                            toggleSize: 15.0,
-                            value: status,
-                            borderRadius: 30.0,
-                            padding: 8.0,
-                            showOnOff: true,
-                            onToggle: (val) {
-                              setState(() {
-                                status = val;
-                              });
-                            },
-                          ),
-
-                        ),
-                      ],
-                      ),
-                      TableRow(children: [
-                        Container (
-                            margin: EdgeInsets.all(10),
-                            padding: EdgeInsets.all(10),
-                            alignment: Alignment.center,
-                            child: Text('Light 2',style: TextStyle( fontSize: 26,))
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          alignment: Alignment.center,
-                          child: FlutterSwitch(
-                            width: 60.0,
-                            height: 25.0,
-                            valueFontSize: 10.0,
-                            toggleSize: 15.0,
-                            value: status1,
-                            borderRadius: 30.0,
-                            padding: 8.0,
-                            showOnOff: true,
-                            onToggle: (val1) {
-                              setState(() {
-                                status1 = val1;
-                              });
-                            },
-                          ),
-
-                        ),
-
-                      ],
-                      ),
-                      TableRow(children: [
-                        Container (
-                            margin: EdgeInsets.all(10),
-                            padding: EdgeInsets.all(10),
-                            alignment: Alignment.center,
-                            child: Text('Speaker',style: TextStyle( fontSize: 26,))
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          alignment: Alignment.center,
-                          child: FlutterSwitch(
-                            width: 60.0,
-                            height: 25.0,
-                            valueFontSize: 10.0,
-                            toggleSize: 15.0,
-                            value: status2,
-                            borderRadius: 30.0,
-                            padding: 8.0,
-                            showOnOff: true,
-                            onToggle: (val1) {
-                              setState(() {
-                                status2 = val1;
-                              });
-                            },
-                          ),
-
-                        ),
-                      ],
-                      )
-                    ],
-                  ),
-                ),
-
+                Container(), //Room2
                 Container(),
                 Container(),
               ],
@@ -308,5 +138,6 @@ class _ControlPageState extends State<ControlPage> {
           ),
         ),
       );
+        // });
     }
 }
--- lib/screen/mainhomepage.dart
+++ lib/screen/mainhomepage.dart
@@ -5,51 +5,94 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:lite_rolling_switch/lite_rolling_switch.dart';
-
 class MainHomePage extends StatefulWidget {
+
   @override
-  _HomePageState createState() => _HomePageState();
+  _MainHomePageState createState() => _MainHomePageState();
 }
 
-class _HomePageState extends State<MainHomePage> {
-  bool relay1pressed = false;
-  final databaseReferenceTest = FirebaseFirestore.instance.collection('noise_feed').doc('qMBoZX6uRcUfkN3eebhi');
-
-@override
+class _MainHomePageState extends State<MainHomePage> {
+  var dataNoise =''  ;
+  // @override
+  // final rowRoom = 
+  @override
   Widget build(BuildContext context) {
-    var data =''  ;
-  return  StreamBuilder(
+    return StreamBuilder(
     stream: FirebaseFirestore.instance.collection("noise_feed").snapshots(includeMetadataChanges: true),
     builder: (BuildContext context,
-              AsyncSnapshot snapshot) {  
-                 
-                if (snapshot.connectionState == ConnectionState.active){
+              AsyncSnapshot snapshot){
+                 if (snapshot.connectionState == ConnectionState.active){
                    print(snapshot.data.docs[0].data()['value']);
-                    data = snapshot.data.docs[0].data()['value'];
+                    dataNoise = snapshot.data.docs[0].data()['value'];
                 }
-          return Container(
-            child:Row(
-            children:[
-                Text("${data}"),
-            LiteRollingSwitch(
-                  value: relay1pressed,
-                  textOn: 'active',
-                  textOff: 'inactive',
-                  colorOn: Colors.deepOrange,
-                  colorOff: Colors.blueGrey,
-                  iconOn: Icons.lightbulb_outline,
-                  iconOff: Icons.power_settings_new,
-                  onChanged: (bool state) {
-                    state
-                        ? databaseReferenceTest
-                            .update({'value': '1'})
-                        : databaseReferenceTest
-                            .update({'value': '0'}) ;
-                    }    )
-              ]
-            )
-          );
-    }
-  );
-}}
+              return Scaffold(
+                appBar: AppBar (
+                  backgroundColor: Colors.orangeAccent,
+                  title: Text ("Homepage"),
+                ),
+                body: Center ( 
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                       Row (
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        children: <Widget>[
+                          Icon(
+                            Icons.computer,
+                            color: Colors.green,
+                            size: 80.0,
+                          ),
+                          Text('ROOM 1'),
+                         
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        children: <Widget>[
+                          // Icon(
+                          //   Icons.pie_chart,
+                          //   color: Colors.green,
+                          //   size: 80.0,
+                          // ),
+                           Text('${dataNoise}',style: TextStyle(fontSize: 25)),
+                          Text('Light Intensity', textAlign: TextAlign.center),
+                        ],
+                      ),
+                    ),
+
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        children: <Widget>[
+                          // Icon(
+                          //   Icons.pie_chart,
+                          //   color: Colors.green,
+                          //   size: 80.0,
+                          // ),
+                          Text('${dataNoise}',style: TextStyle(fontSize: 25)),
+                          Text('Noise', textAlign: TextAlign.center),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+                      ],
+                    )
+                ),
+    );
+
+
+
+              });
+  }
+}
+
+
